5daea35e40c77a193898377fee17a90db469c462
Eduardo Barbosa (an4kein) <37910997+an4kein@users.noreply.github.com>
2020-02-12 00:55:31 +0000

    Update configure_tmux_and_plugins.py

--- configure_tmux_and_plugins.py
+++ configure_tmux_and_plugins.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # by an4kein
+
 import os
 from os import path
+
+os.system('sudo apt install python-pip -y')
+package = "wget"
+try:
+    import wget
+except:
+    os.system("sudo pip install " + package)
 
 def main():
     if  path.isfile('.tmux.conf') == True:
@@ -9,10 +17,10 @@ def main():
     elif path.isfile('.tmux.conf') == False:
         print("Download .tmux.conf and TMUX-PLUGINS")
         if path.isdir('/opt/tmux-plugins/') == True:
-            print("Exist plugins into /opt/")
+            print("Exist plugins into /opt/ download only .tmux.conf")
+            os.system('sudo wget https://raw.githubusercontent.com/an4kein/tmux/master/.tmux.conf')
         elif path.isdir('/opt/tmux-plugins') == False:
             os.system('sudo git clone https://github.com/tmux-plugins/tmux-logging.git /opt/tmux-plugins/')
-            url = 'https://raw.githubusercontent.com/an4kein/tmux/master/.tmux.conf'
             os.system('sudo wget https://raw.githubusercontent.com/an4kein/tmux/master/.tmux.conf')
 
 if __name__== "__main__":
